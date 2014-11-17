4dd8f3d493585ae79d1fac74068a7e1bfd7e79ed
Adrien Folie <folie.adrien@gmail.com>
2014-11-17 17:54:36 +0100

    refactor stream test

--- test/stream.js
+++ test/stream.js
@@ -11,12 +11,15 @@ describe('WritableStream', function() {
                 expect(data).to.equal(msg);
                 done();
             });
-
+            writer.write(message());
+        });
+        
+        function message() {
             var bytes = [];
             for (var i = 0; i < msg.length; ++i) {
                 bytes.push(msg.charCodeAt(i));
             }
-            writer.write(bytes);
-        });
+            return bytes;
+        }
     });
 });
