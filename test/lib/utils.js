13ca0d0ab8733fdbbf10a28e60e125e2ebfa5ef8
Adrien Folie <folie.adrien@gmail.com>
2015-04-11 05:08:35 +0200

    fix typo mistake

--- test/lib/utils.js
+++ test/lib/utils.js
@@ -32,7 +32,7 @@ describe('Utils', function() {
         });
 
         context('when no language is specified', function() {
-            it("doesn't fail" function() {
+            it("doesn't fail", function() {
                 expect(function() {
                     utils.formatImage('grounds');
                 }).not.to.throw(Error);
@@ -40,7 +40,7 @@ describe('Utils', function() {
         });
 
         context('when no tag is specified', function() {
-            it("doesn't fail" function() {
+            it("doesn't fail", function() {
                 expect(function() {
                     utils.formatImage('grounds', 'ruby');
                 }).not.to.throw(Error);
